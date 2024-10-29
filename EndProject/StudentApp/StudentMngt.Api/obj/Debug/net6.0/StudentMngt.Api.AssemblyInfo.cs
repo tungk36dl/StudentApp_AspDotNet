e42384fe68ab43c86f2237bb91dade1ee18a8985
Pham The Tung <tungxeco55@gmail.com>
2024-10-29 13:49:10 +0700

    test commit gitignore

--- EndProject/StudentApp/StudentMngt.Api/obj/Debug/net6.0/StudentMngt.Api.AssemblyInfo.cs
+++ EndProject/StudentApp/StudentMngt.Api/obj/Debug/net6.0/StudentMngt.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentMngt.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3eb1117c1bfda3353f9481e16fbd2e05a4f554c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentMngt.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentMngt.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- EndProject/StudentApp/StudentMngt.Persistence/obj/Debug/net6.0/StudentMngt.Persistence.AssemblyInfo.cs
+++ EndProject/StudentApp/StudentMngt.Persistence/obj/Debug/net6.0/StudentMngt.Persistence.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentMngt.Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e05d134ee609d13c387485c706a61b6cfa5a7cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3eb1117c1bfda3353f9481e16fbd2e05a4f554c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentMngt.Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentMngt.Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
